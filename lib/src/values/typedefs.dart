07881615f0f34f4c4aceb9142d10a720b9ee1c08
dhyash-simform <yash.dhrangdhar@simformsolutions.com>
2025-08-11 10:01:34 +0530

    fix: :hammer: Handle incorrect index if sorting applied

--- lib/src/values/typedefs.dart
+++ lib/src/values/typedefs.dart
@@ -158,3 +158,7 @@ typedef PaginationCallback = Future<void> Function(
 typedef OldReplyMessageFetchCallback = Future<void> Function(
   String messageId,
 );
+typedef PaginationScrollUpdateResult = ({
+  ChatPaginationDirection? direction,
+  Message? message,
+});
